3747c34c0e39a97149fb59f88588dbc5aa70193d
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-03-11 18:54:07 +0530

    Using async/await

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import Places from "./Places.jsx";
 
@@ -6,14 +6,13 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-    .then((response) => {
-      return response.json();
-    })
-    .then((resData) => {
+    async function fetchPlaces() {
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
       setAvailablePlaces(resData.places);
-    });
-
+    }
+    
+    fetchPlaces();
   }, []);
 
   return (
