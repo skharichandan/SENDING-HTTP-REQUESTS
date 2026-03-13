517e5094769029ea991d12123830c277a0acbfa2
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-03-13 12:19:05 +0530

    Transforming Fetched Data

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -20,12 +21,19 @@ export default function AvailablePlaces({ onSelectPlace }) {
           throw new Error("Failed to fetch places.");
         }
 
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude,
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({ message: error.message || "Something went wrong!" });
+        setIsFetching(false);
       }
-
-      setIsFetching(false);
     }
 
     fetchPlaces();
